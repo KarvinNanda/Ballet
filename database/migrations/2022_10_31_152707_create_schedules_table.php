e1756a56bbfa76fdcd1febe18d90d0e066377ad1
Saminjo <95714949+Saminjo@users.noreply.github.com>
2022-11-22 23:24:44 +0700

    komit pertama

---
+++ database/migrations/2022_10_31_152707_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('teacher_id');
+            $table->unsignedBigInteger('class_id');
+            $table->string('hari');
+            $table->string('jam');
+
+            $table->foreign('class_id')->references('id')->on('kelas')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
