5fe781e263323deb09e69eaf6771a582206ba308
Karvin <108486623+KarvinNanda@users.noreply.github.com>
2022-12-11 20:39:18 +0700

    Add files via upload
    add file seeder

---
+++ database/seeders/RekeningSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RekeningSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('rekenings')->insert([
+            [
+                'bank_rek' => '1978646789',
+                'banks_id' => '2',
+                'nama_pengirim' => 'Udin'
+            ],
+
+            [
+                'bank_rek' => '5271992837',
+                'banks_id' => 1,
+                'nama_pengirim' => 'Budi'
+            ],
+
+        ]);
+    }
+}
