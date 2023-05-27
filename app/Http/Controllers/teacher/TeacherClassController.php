da62826860a089ba9eee8d6b07041aef6b45ce74
felixgoz <felixgoo60@gmail.com>
2023-05-27 11:50:30 +0700

    done2

--- app/Http/Controllers/teacher/TeacherClassController.php
+++ app/Http/Controllers/teacher/TeacherClassController.php
@@ -12,110 +12,118 @@ use Illuminate\Support\Facades\DB;
 
 class TeacherClassController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $data = DB::table('class_transactions')
-            ->join('mapping_class_teachers','mapping_class_teachers.class_id','class_transactions.id')
-            ->join('mapping_class_children','mapping_class_children.class_id','class_transactions.id')
-            ->join('class_types','class_transactions.class_type_id','class_types.id')
+            ->join('mapping_class_teachers', 'mapping_class_teachers.class_id', 'class_transactions.id')
+            ->join('mapping_class_children', 'mapping_class_children.class_id', 'class_transactions.id')
+            ->join('class_types', 'class_transactions.class_type_id', 'class_types.id')
             ->selectRaw('
                 class_transactions.id as id,
                 class_types.class_name as class,
                 COUNT(mapping_class_children.class_id) as students
             ')
-            ->where('mapping_class_teachers.user_id',Auth::user()->id)
+            ->where('mapping_class_teachers.user_id', Auth::user()->id)
             ->groupBy(['class'])
             ->get()
             ->groupBy('class');
-        return view('teacher.class.index',compact('data'));
+        return view('teacher.class.index', compact('data'));
     }
 
-    public function viewDetail(Request $request){
+    public function viewDetail(Request $request)
+    {
         $data = DB::table('class_transactions')
-            ->join('mapping_class_children','mapping_class_children.class_id','class_transactions.id')
-            ->join('students','mapping_class_children.student_id','students.id')
+            ->join('mapping_class_children', 'mapping_class_children.class_id', 'class_transactions.id')
+            ->join('students', 'mapping_class_children.student_id', 'students.id')
             ->selectRaw('
                 students.LongName as student_name,
                 YEAR(CURDATE()) - YEAR(students.dob) as student_old,
                 students.dob as student_dob
             ')
-            ->where('mapping_class_children.class_id',$request->id)
+            ->where('mapping_class_children.class_id', $request->id)
             ->get();
-        return view('teacher.class.detail',compact('data'));
+        return view('teacher.class.detail', compact('data'));
     }
 
-    public function viewSchedule(Request $req){
+    public function viewSchedule(Request $req)
+    {
         $classId = $req->classId;
         $class = DB::table('schedules')
-            ->join('class_transactions','class_transactions.id','schedules.class_id')
+            ->join('class_transactions', 'class_transactions.id', 'schedules.class_id')
             ->selectRaw('
                 schedules.date as date,
                 schedules.id as id
-            ')->where('schedules.class_id',$req->classId)->orderBy("date")
+            ')->where('schedules.class_id', $req->classId)->orderBy("date")
             ->get();
-        return view('teacher.class.viewSchedule',compact('class','classId'));
+        return view('teacher.class.viewSchedule', compact('class', 'classId'));
     }
 
-    public function deleteScheduleClass($id,$classId){
-        $classDelete = DB::table('schedules')->where('schedules.id',$id)->where('class_id',$classId);
+    public function deleteScheduleClass($id, $classId)
+    {
+        $classDelete = DB::table('schedules')->where('schedules.id', $id)->where('class_id', $classId);
         $classDelete->delete();
-        return redirect()->route("viewClass");
+        return redirect()->route("viewAllScheduleTeacher", ['userId' => auth()->id()]);
     }
 
-    public function viewUpdateScheduleClass(Request $req){
+    public function viewUpdateScheduleClass(Request $req)
+    {
         $schedule = Schedule::find($req->scheduleId);
-        return view('teacher.class.viewUpdateSchedule',compact('schedule'));
+        return view('teacher.class.viewUpdateSchedule', compact('schedule'));
     }
 
-    public function updateSchedule(Request $req){
+    public function updateSchedule(Request $req)
+    {
         $schedule = Schedule::find($req->scheduleId);
         $schedule->date = Carbon::parse($req->dateTime);
         $schedule->save();
-        return redirect()->route("viewClass");
+        return redirect()->route("viewAllScheduleTeacher", ['userId' => auth()->id()]);
     }
 
-    public function viewaddScheduleClass(Request $req){
+    public function viewaddScheduleClass(Request $req)
+    {
         $classId = $req->classId;
         $test = Schedule::find($classId);
-        return view('teacher.class.viewaddSchedule',compact('classId'));
+        return view('teacher.class.viewaddSchedule', compact('classId'));
     }
 
-    public function viewAddMultipleScheduleClass(Request $req){
+    public function viewAddMultipleScheduleClass(Request $req)
+    {
         $classId = $req->classId;
         $test = Schedule::find($classId);
-        return view('teacher.class.addMultipleSchedule',compact('classId'));
+        return view('teacher.class.addMultipleSchedule', compact('classId'));
     }
 
-    public function addSchedule(Request $req){
+    public function addSchedule(Request $req)
+    {
         $date = Carbon::parse($req->dateTime);
-        $class_schedule = Schedule::where('class_id',$req->classId)->get();
+        $class_schedule = Schedule::where('class_id', $req->classId)->get();
         $bool = true;
-        foreach ($class_schedule as $sche){
-            if(Carbon::parse($sche->date)->toDateString()==Carbon::parse($date)->toDateString())
-            {
+        foreach ($class_schedule as $sche) {
+            if (Carbon::parse($sche->date)->toDateString() == Carbon::parse($date)->toDateString()) {
                 $hour = Carbon::parse($date)->diff(Carbon::parse($sche->date))->format('%H');
                 $num = (int)$hour;
-                if($num<1)
-                {
-                    $bool=false;
+                if ($num < 1) {
+                    $bool = false;
                 }
             }
         }
-        if($bool==false){
-            return redirect()->route("viewClass");
-        }else{
+        if ($bool == false) {
+            return redirect()->route("viewAllScheduleTeacher", ['userId' => auth()->id()]);
+        } else {
             $schedule = new Schedule();
             $schedule->class_id = $req->classId;
             $schedule->date = $date;
             $schedule->save();
         }
-        return redirect()->route("viewClass");
+        return redirect()->route("viewAllScheduleTeacher", ['userId' => auth()->id()]);
     }
 
-    public function addMultipleSchedule(Request $req){
+    public function addMultipleSchedule(Request $req)
+    {
 
         $date = Carbon::parse($req->dateTime);
 
-        for($i =0;$i < $req->ScheduleLoop;$i++){
+        for ($i = 0; $i < $req->ScheduleLoop; $i++) {
             $schedule = new Schedule();
             $schedule->class_id = $req->classId;
             $schedule->date = $date;
@@ -123,6 +131,15 @@ class TeacherClassController extends Controller
             $date->addDay(7);
         }
 
-        return redirect()->route("viewClass");
+        return redirect()->route("viewAllScheduleTeacher", ['userId' => auth()->id()]);
+    }
+
+    public function viewClassSchedule(Request $request)
+    {
+        $userId = $request->input('userId');
+        $classes = ClassTransaction::whereHas('mapping', function ($query) use ($userId) {
+            $query->where('user_id', $userId);
+        })->where('Status', 'aktif')->simplePaginate(5);
+        return view('teacher.class.schedule', compact('classes'));
     }
 }
